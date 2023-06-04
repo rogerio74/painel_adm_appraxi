77717d2ec8d8ef05fddd816b5d857909be5c3102
Werton Pereira <95698369+MrWerton@users.noreply.github.com>
2023-06-04 18:47:41 -0300

    Update firebase_licao.tsx

--- src/common/services/firebase_licao.tsx
+++ src/common/services/firebase_licao.tsx
@@ -2,15 +2,6 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
-const firebaseConfig2 = {
-  apiKey: 'AIzaSyDNNgEO8OCi2GJyNob-C14FC-KlzfFJSMc',
-  authDomain: 'captura-vozes.firebaseapp.com',
-  projectId: 'captura-vozes',
-  storageBucket: 'captura-vozes.appspot.com',
-  messagingSenderId: '36551383721',
-  appId: '1:36551383721:web:cf86bc08e332c41895a7b8',
-  measurementId: 'G-WDN7R7WC06'
-}
 
 const appAudio = initializeApp(firebaseConfig2, 'secondary')
 
